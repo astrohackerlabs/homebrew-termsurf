e6f2a632872f7b75dc5923dd953600ca0ba5b8d1
Ryan X. Charles <ryan@ryanxcharles.com>
2026-06-19 17:37:43 -0500

    v0.1.6

--- Casks/termsurf.rb
+++ Casks/termsurf.rb
@@ -1,31 +1,31 @@
 cask "termsurf" do
-  version "0.1.5"
-  sha256 "aef810a53fbb93427c69a48f2459a83b22ed75ae403140d8bfcac0dad1618959"
+  version "0.1.6"
+  sha256 "2cecf0a518b087b6feb59f8d37203cde6b3d411b59e430234b21e7bad74e2016"
 
   url "https://github.com/termsurf/termsurf/releases/download/v#{version}/termsurf-#{version}-aarch64-apple-darwin.tar.gz"
   name "TermSurf"
-  desc "A protocol for embedding web browsers inside terminal emulators"
-  homepage "https://termsurf.com"
+  desc "Protocol for embedding web browsers inside terminal emulators"
+  homepage "https://termsurf.com/"
 
-  app "TermSurf Wezboard.app"
+  depends_on macos: :sequoia
+
+  app "TermSurf.app"
   binary "web"
-  binary "wezboard"
-
   artifact "roamium", target: "/opt/homebrew/opt/termsurf-roamium"
 
   postflight do
+    app_path = "#{appdir}/TermSurf.app"
+
     system_command "codesign", args: ["--force", "--sign", "-", staged_path/"web"]
-    system_command "codesign", args: ["--force", "--sign", "-", staged_path/"wezboard"]
     system_command "codesign", args: ["--force", "--sign", "-", "/opt/homebrew/opt/termsurf-roamium/roamium"]
     system_command "codesign",
                    args: ["--force", "--deep", "--sign", "-",
-                          "/Applications/TermSurf Wezboard.app"]
+                          app_path]
     # Clear quarantine on everything — the tarball propagates the attribute
     # to all extracted files, and Gatekeeper blocks unsigned binaries
-    system_command "xattr", args: ["-cr", "/Applications/TermSurf Wezboard.app"]
+    system_command "xattr", args: ["-cr", app_path]
     system_command "xattr", args: ["-cr", "/opt/homebrew/opt/termsurf-roamium"]
     system_command "xattr", args: ["-cr", staged_path/"web"]
-    system_command "xattr", args: ["-cr", staged_path/"wezboard"]
   end
 
   zap trash: [
